75b7aeaefc607a1eb2062c74b9f2344a2c2810c1
Bishode123 <34882617+Bishode123@users.noreply.github.com>
2017-12-29 14:16:31 +0530

    FirstCommit

--- ViewModel29thDecember/ViewModel/vmStudMarks.cs
+++ ViewModel29thDecember/ViewModel/vmStudMarks.cs
@@ -7,7 +7,6 @@ namespace ViewModel29thDecember.ViewModel
 {
     public class vmStudMarks
     {
-        
             public string Name { get; set; }
             public string Marks { get; set; }
             public string colorname { get; set; }
